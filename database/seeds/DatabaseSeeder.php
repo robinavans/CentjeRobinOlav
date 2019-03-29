99b468fc48a93254645e6f4114335754d9da0bfc
Robin van den Udenhout <rlmudenh@avans.nl>
2019-03-29 14:00:57 +0100

    goede seeddata

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,27 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Robin',
-            'email' => 'Robin'.'@gmail.com',
-            'password' => bcrypt('secret'),
-        ]);
-        DB::table('betaal_verzoekens')->insert([
-            'Userid' => '1',
-            'Rekeningid' => '1',
-            'name' => 'Eten',
-            'description' => 'Zou je dit willen betalen?',
-            'amount' => '100',
-            'amountpaid' => 0,
-            'valuta' => 'Euro',
-            'created_at' => '2019-03-27 13:30:16',
-            'updated_at' => '2019-03-27 13:30:16',
-        ]);
-        DB::table('rekeningens')->insert([
-            'Userid' => '1',
-            'name' => 'Eerste rekening',
-            'rekeningnummer' => '1234SNSB2023289328942',
-            'vermogen' => '100',
+        $this->call([
+            UsersTableSeeder::class,
+            BetaalvezoekensTableSeeder::class,
+            BetalingsTableSeeder::class,
+            RekeningenTableSeeder::class,
         ]);
     }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Robin',
+            'email' => 'Robin'.'@gmail.com',
+            'password' => bcrypt('robin12345'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Olav',
+            'email' => 'Olav'.'@gmail.com',
+            'password' => bcrypt('olav123456'),
+        ]);
+    }
+}
